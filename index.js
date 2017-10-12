8c98a4ff2805feae8a1400dc670bccd1bade4a6c
Himanshu Attri <attri.him@gmail.com>
2017-10-13 01:59:04 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ var express = require('express');
 var app = express();
 var app1 = express();
 
+app.set('port', (process.env.PORT || 3009));
 
+app.use(express.static(__dirname + '/public'));
 
-app1.set('port',(process.env.PORT || 5000));
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs');
 
-app1.use(express.static(__dirname + '/public'));
-app1.set('views', __dirname + ,'/views'));
-app1.set('view engine', 'ejs');
-
-
-app1.get('/', function(req,res){
-	res.json({'value':'key'})
-
+app.get('/', function(req, res){
+	res.json({ 'message': 'sfsdsd'})
 });
 
-app1.listen(app1.get('port'), function(){
-	console.log("RunningOk on: ",app1.get('port'));
+
+
+app.get('/', function() {
+	console.log('Running', app.get('port'));
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
